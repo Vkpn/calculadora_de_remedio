18de744e3b625c6baeb49e4936c6906ef0956e77
Vkpn <victoraso10@hotmail.com>
2022-02-14 19:03:56 -0300

    update js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var hora = document.getElementById(`fhora`)
 var intervalo = document.getElementById(`fintervalo`)
 var dias = document.getElementById(`fdias`)
 var res = document.getElementById(`fres`)
-var lista = []
+var imp = document.getElementById(`imprimir`)
 
-function gravar(){ // validaçao de erro
+function gravar(){ // validaçao de erro no formulario
     if(remedio.value == `` || hora.value == `` || intervalo.value == `` || dias.value == `` || intervalo.value <= 0 || intervalo.value >= 25 || dias.value < 0){
         if (remedio.value == ``) {
             alert(`[ERRO] Preencher o nome do remédio!`)
@@ -24,129 +24,90 @@ function gravar(){ // validaçao de erro
             dias.focus()
         }
     } else {  
-        var item = document.createElement(`p`) // Impressao do nome do remedio
-        item.innerHTML = `${remedio.value.toUpperCase()}:`
+        var item = document.createElement(`h3`) // Impressao do nome do remedio
+        item.innerHTML = `&#128138 ${remedio.value.toUpperCase()}:`
         res.appendChild(item)
 
         var proxh = parseInt(hora.value.toString()) // Impressao do horario ao lado do dia do remedio
-        var int = parseInt(intervalo.value)
-        horalimite = proxh + int  
-
-        
-        // VVVVVVVVVVVVVVVVv
-
-        for(var d = 1; d <= Number(dias.value); d++){ //impressao dos dias a serem tomado o remedio
-            var itemdia = document.createElement(`p`)
-            itemdia.innerHTML = `${d}° dia:`
-            res.appendChild(itemdia)
-        }
-
-        for(proxh; proxh < 24; proxh += int){
-            var itemhora = document.createElement(`p`)
-            itemhora.innerHTML += `${proxh}`
-            res.appendChild(itemhora)
-        }
-
-
-        /*
-        for(var i=0; i<10; i++){
-            for(var j=0; j<10; j++){
-               document.body.innerHTML += i + String.fromCharCode( j + 64 ) + ' ';
-            }
-            document.body.innerHTML += '<br>'; // quebra de linha pra facilitar a leitura
-         }
-         */
-
-         //----------
-
-        /*
-        for(var d = 1; d <= Number(dias.value); d++){ //impressao dos dias a serem tomado o remedio
-            var itemdia = document.createElement(`p`)
-            itemdia.innerHTML = `${d}° dia:`
-            res.appendChild(itemdia)
-        }
-        
-        if(d <= Number(dias.value)){
-            var d = 1
-            var itemdia = document.createElement(`p`)
-            itemdia.innerHTML = `${d}° dia:`
-            res.appendChild(itemdia)
-            d++
-        } else if (proxh < 24) {
-            var itemhora = document.createElement(`p`)
-            itemhora.innerHTML += `${proxh}`
-            res.appendChild(itemhora)
-            
-        }
-        */
-
-        //------------
-
-        /*
-
-        for(proxh; proxh < 24; proxh += int){
-            var itemhora = document.createElement(`p`)
-            itemhora.innerHTML += `${proxh}`
-            res.appendChild(itemhora)
-        }
-        */
-
-        //------------
-
-        /*
-        if(horalimite < 24){
-            alert(horalimite)
-        } else {
-            alert(`ok`)
-        }
-        */
-
-        //---------
-        
-        /*
-        var d = new Date()
-        var hrs = hora.value.split(`:`)[0]
         var min = hora.value.split(`:`)[1]
+        var int = parseInt(intervalo.value)
+        var horalimite = proxh + int  
 
-        d.setHours(hrs)
-        d.setMinutes(min)
+        for(var d = 1; d <= Number(dias.value); d++){ //impressao dos dias a serem tomado o remedio
+            
+            var itemdia = document.createElement(`p`)
+            itemdia.innerHTML = `${d}° dia: `
+            res.appendChild(itemdia)
 
-        var total = d.getHours()+ `:` + d.getMinutes()
-        alert(total)
-        alert(int)
-        */
+           if(d == 1){ // teste do primeiro dia
 
+                for(var proxh = parseInt(hora.value.toString()); proxh < 24; proxh += int){ //impressao dos horarios a tomar o remedio
+                    
+                    var itemhora = document.createElement(`p`)
+                    itemhora.innerHTML +=`&#128306 ${proxh}:${min} | `
+                    itemhora.style.display = `inline-block`
+                    itemhora.style.backgroundColor = `rgba(255, 255, 255, 0)`
+                    itemdia.appendChild(itemhora)
+                }
 
-        //------
+            } else if (d > 1 && horalimite < 24) { // teste dos demais dias com hora normal
 
-        //var hrs = hora.value.split(`:`)[0]
-        //var min = hora.value.split(`:`)[1]
-        //var int = intervalo.value
+                for(proxh = horalimite - 24; proxh < 24; proxh += int){ //impressao dos horarios a tomar o remedio
+                    
+                    if(proxh < 0){ // validaçao de numero negativo
+                    }else{
+                        var itemhora = document.createElement(`p`)
+                        itemhora.innerHTML += `&#128306 ${proxh}:${min} | `
+                        itemhora.style.display = `inline-block`
+                        itemhora.style.backgroundColor = `rgba(255, 255, 255, 0)`
+                        itemdia.appendChild(itemhora)
+                    }
+                }
 
-        //alert(hrs)
+            } else if (d > 1 && horalimite >= 24){ // teste dos demais dias com hora acim de 24
+                
+                for(proxh = horalimite - 24; proxh < 24; proxh += int){ //impressao dos horarios a tomar o remedio
+                    
+                    if(proxh < 0){ // validaçao de numero negativo
+                    }else{
+                        var itemhora = document.createElement(`p`)
+                        itemhora.innerHTML += `&#128306 ${proxh}:${min} | `
+                        itemhora.style.display = `inline-block`
+                        itemhora.style.backgroundColor = `rgba(255, 255, 255, 0)`
+                        itemdia.appendChild(itemhora)
+                    }
+                }
+                
+            }
 
-
-        //------
-
-        //var h = hora.value
-        //var vh = h.valueOf()
-
-        //var i = intervalo.value
-        //var vi = i.valueOf()
-
-        //-----
-
-        //alert(String(hora.value))
+        }
 
     }
 } 
 
-/* 
-terminar formulário de resposta
-calculo de horários
-adicionar botão imprimir
-alterar barra de rolagem
-*/
+function imprimir(){ // imprimir
+    if(remedio.value == `` || hora.value == `` || intervalo.value == `` || dias.value == `` || intervalo.value <= 0 || intervalo.value >= 25 || dias.value < 0){
+        if (remedio.value == ``) {
+            alert(`[ERRO] Preencher o nome do remédio!`)
+            remedio.focus()
+        } else if (hora.value == ``) {
+            alert(`[ERRO] Preencher o horário corretamente!`)
+            hora.focus()
+        } else if (intervalo.value == ``) {
+            alert(`[ERRO] Preencher o intevalo corretamente!`)
+            intervalo.focus()
+        } else if (intervalo.value <= 0 || intervalo.value >= 25) {
+            alert(`[ERRO] intervalo deve ser entre 1 a 24 horas.`)
+            intervalo.focus()
+        } else if (dias.value == `` || dias.value < 0) {
+            alert(`[ERRO] Preencher os dias corretamente!`)
+            dias.focus()
+        }
+    } else {  
+        var dados = document.getElementById(`fres`).innerHTML
+        var imprimir = window.open()
+        imprimir.document.write(innerHTML = `<p style="text-align: center;"> Obrigado por utilizar meu site! </p>` + `<div style="font-size: 1vh; padding: 0px 100px">${dados}</div>` + `<p style="text-align: center;">Tome seus remédios no horario certo, acompanhe o tratamento junto ao seu médico de confiança e MELHORAS!!`)
+        imprimir.print()
+    }
 
-
-
+}
