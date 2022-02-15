82b603915ea039e0fb0ca8b61dd334d72ddc4277
Vkpn <victoraso10@hotmail.com>
2022-02-14 22:39:42 -0300

    att site
    arrumar estilo e add features

--- script.js
+++ script.js
@@ -83,31 +83,26 @@ function gravar(){ // validaçao de erro no formulario
         }
 
     }
+    
+    document.getElementById(`fremedio`).value=`` // limpesa do formulario
+    document.getElementById(`fhora`).value=``
+    document.getElementById(`fintervalo`).value=``
+    document.getElementById(`fdias`).value=``
+    imp.disabled = false
+    remedio.focus()
 } 
 
-function imprimir(){ // imprimir
-    if(remedio.value == `` || hora.value == `` || intervalo.value == `` || dias.value == `` || intervalo.value <= 0 || intervalo.value >= 25 || dias.value < 0){
-        if (remedio.value == ``) {
-            alert(`[ERRO] Preencher o nome do remédio!`)
-            remedio.focus()
-        } else if (hora.value == ``) {
-            alert(`[ERRO] Preencher o horário corretamente!`)
-            hora.focus()
-        } else if (intervalo.value == ``) {
-            alert(`[ERRO] Preencher o intevalo corretamente!`)
-            intervalo.focus()
-        } else if (intervalo.value <= 0 || intervalo.value >= 25) {
-            alert(`[ERRO] intervalo deve ser entre 1 a 24 horas.`)
-            intervalo.focus()
-        } else if (dias.value == `` || dias.value < 0) {
-            alert(`[ERRO] Preencher os dias corretamente!`)
-            dias.focus()
-        }
-    } else {  
-        var dados = document.getElementById(`fres`).innerHTML
-        var imprimir = window.open()
-        imprimir.document.write(innerHTML = `<p style="text-align: center;"> Obrigado por utilizar meu site! </p>` + `<div style="font-size: 1vh; padding: 0px 100px">${dados}</div>` + `<p style="text-align: center;">Tome seus remédios no horario certo, acompanhe o tratamento junto ao seu médico de confiança e MELHORAS!!`)
-        imprimir.print()
-    }
+function imprimir(){ // funcao imprimir
+
+    var dados = document.getElementById(`fres`).innerHTML
+    var imprimir = window.open()
+    imprimir.document.write(innerHTML = `<p style="text-align: center;"> Obrigado por utilizar meu site! </p>` + `<div style="font-size: 1vh; padding: 0px 100px">${dados}</div>` + `<p style="text-align: center;">Tome seus remédios no horario certo, acompanhe o tratamento junto ao seu médico de confiança e MELHORAS!!`)
+    imprimir.print()
+
+}
+
+function reset(){ // funcao reset
+    var btn = document.getElementById(`reset`)
+    btn.addEventListener(`click`, function(){location.reload()})
 
 }
